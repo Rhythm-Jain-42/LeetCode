cad71ea3cb5db8ca7facd0be4b96d1b7cce964eb
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-06-08 20:00:27 +0530

    Time: 11 ms (37.51%), Space: 13 MB (75.68%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        queue<pair<pair<int,int>, int>> q;
+        int vis[n][m];
+        int fresh=0;
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
+                }
+                else{
+                    vis[i][j]=0;
+                }
+                
+                if(grid[i][j]==1)
+                {
+                    fresh++;
+                }
+            }
+        }
+        
+        int ans=0;
+        int row[]={-1, 0, 1, 0};
+        int col[]={0, 1, 0, -1};
+        int count=0;
+        
+        while(!q.empty())
+        {
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            q.pop();
+            ans=t;
+            
+            for(int i=0; i<4; i++)
+            {
+                int nrow=r+row[i];
+                int ncol=c+col[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1)
+                {
+                    q.push({{nrow, ncol}, t+1});
+                    vis[nrow][ncol]=2;
+                    count++;
+                }
+            }
+        }
+        
+        if(count!=fresh) return -1;
+        return ans;
+    }
+};
