61145ba98417776cbc8e1f2312bab81c7f39982d
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-06-14 14:09:14 +0530

    Time: 23 ms (70.46%), Space: 13.9 MB (55.34%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int V=numCourses;
+        vector<int> adj[V];
+        for(auto it :prerequisites)
+            adj[it[1]].push_back(it[0]);
+        
+        vector<int> inDegree(V, 0);
+        for(int i=0; i<V; i++)
+        {
+            for(auto it :adj[i])
+                inDegree[it]++;
+        }
+        
+        queue<int> q;
+        for(int i=0; i<V; i++)
+        {
+            if(inDegree[i]==0)
+                q.push(i);
+        }
+        
+        vector<int> topo;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            
+            for(auto it :adj[node])
+            {
+                inDegree[it]--;
+                if(inDegree[it]==0)
+                    q.push(it);
+            }
+        }
+        
+        if(topo.size()==V) return topo;
+        return {};
+    }
+};
