5e4d1e6e07ecff943150a00dc7b3655d55772199
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-06-19 16:23:15 +0530

    Time: 6 ms (28.46%), Space: 7.9 MB (54.63%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int ans=0;
+        int curr=0;
+        for(int i=0;i<gain.size();i++){
+            curr+=gain[i]; 
+            ans=max(curr, ans);
+        }
+        return ans;
+    }
+};
