bfec9f031b2a710c59b78dd1ecf154274a5cd250
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-07-08 15:22:01 +0530

    Time: 9 ms (73.47%), Space: 10.2 MB (14.80%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -17,6 +17,26 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(m-1, n-1, grid, dp);
+        
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                else
+                {
+                    int up=grid[i][j];
+                    if(i>0) up+=dp[i-1][j];
+                    else up+=1e9;
+                    
+                    int left=grid[i][j];
+                    if(j>0) left+=dp[i][j-1];
+                    else left+=1e9;
+                    
+                    dp[i][j]=min(up, left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
