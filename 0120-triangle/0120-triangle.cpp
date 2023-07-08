c0f65507986cf6c121f41bc531d5eb4ce7a27105
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-07-08 15:40:28 +0530

    Time: 8 ms (56.97%), Space: 8.9 MB (48.42%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -15,6 +15,18 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(0, 0, triangle, n, dp);
+        for(int j=0; j<n; j++) dp[n-1][j]=triangle[n-1][j];
+        
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=i; j>=0; j--)
+            {
+                int d=dp[i+1][j]+triangle[i][j];
+                int dg=dp[i+1][j+1]+triangle[i][j];
+                
+                dp[i][j]=min(d,dg);
+            }
+        }
+        return dp[0][0];
     }
 };
