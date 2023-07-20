ec188de766d6506222942c022127ef5ca4a83687
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-07-20 14:47:23 +0530

    Time: 3 ms (94.68%), Space: 13.7 MB (70.91%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -14,7 +14,16 @@ public:
     
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int> dp(n, -1);
-        return min(f(n-1, cost, dp), f(n-2, cost, dp));
+        vector<int> dp(n);
+        
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        
+        for(int i=2; i<n; i++)
+        {
+            dp[i]=cost[i]+min(dp[i-1], dp[i-2]);
+        }
+        
+        return min(dp[n-1], dp[n-2]);
     }
 };
