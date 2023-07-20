e233382b31504d81c930818da07fb842e7eb9203
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-07-20 23:36:10 +0530

    Time: 425 ms (59.93%), Space: 132.5 MB (63.34%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int f(int ind, int buy, int cap, vector<int>& prices, vector<vector<vector<int>>> &dp)
+    int f(int ind, int transaction, int k, vector<int>& prices, vector<vector<int>> &dp)
     {
-        if(ind==prices.size() || cap==0) return 0;
+        if(ind==prices.size() || transaction==2*k) return 0;
         
-        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
+        if(dp[ind][transaction]!=-1) return dp[ind][transaction];
         
-        if(buy)
+        if(transaction%2==0)  //buy
         {
-            return dp[ind][buy][cap]=max(-prices[ind]+f(ind+1, 0, cap, prices, dp), f(ind+1, 1, cap, prices, dp));
+            return dp[ind][transaction]=max(-prices[ind]+f(ind+1, transaction+1, k, prices, dp), f(ind+1, transaction, k, prices, dp));
         }
         else
         {
-            return dp[ind][buy][cap]=max(prices[ind]+f(ind+1, 1, cap-1, prices, dp), f(ind+1, 0, cap, prices, dp));
+            return dp[ind][transaction]=max(prices[ind]+f(ind+1, transaction+1, k, prices, dp), f(ind+1, transaction, k, prices, dp));
         }
     }
     
     int maxProfit(vector<int>& prices) {
+        int k=2;
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return f(0, 1, 2, prices, dp);
+        vector<vector<int>> dp(n, vector<int>(2*k, -1));
+        return f(0, 0, k, prices, dp);
     }
 };
