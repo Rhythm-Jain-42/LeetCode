23d73379d29fe668c6bda8956581c89d0e9e5e6c
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-08-10 00:41:02 +0530

    Time: 33 ms (78.50%), Space: 21.9 MB (20.33%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp; 
+        
+        for(auto x: strs){
+            string temp=x; 
+            sort(x.begin(), x.end()); 
+            mp[x].push_back(temp); 
+        }
+        vector<vector<string>>ans;
+        for(auto it: mp){
+            vector<string> temp=it.second; 
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
