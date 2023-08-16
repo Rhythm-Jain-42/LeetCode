b13d5f78dbed158290fb355ac82733d7d6bdf52f
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-08-16 23:18:04 +0530

    Time: 177 ms (99.34%), Space: 134.6 MB (89.46%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+         deque<int>d;
+         vector<int>a;
+       for(int i=0;i<nums.size();i++)
+       {
+           if(!d.empty() && d.front()==i-k)
+           {
+                d.pop_front();
+           }
+
+           while(!d.empty() && nums[d.back()]<=nums[i])
+           {
+               d.pop_back();
+           }
+           d.push_back(i);
+           
+           if(i>=k-1)
+           {
+               a.push_back(nums[d.front()]);
+           }
+       }
+        return a;
+        
+    }
+};
