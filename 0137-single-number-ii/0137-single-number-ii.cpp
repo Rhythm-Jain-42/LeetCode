99f05406751af5a8a9e08cb8379f5c6bb9ba1f5a
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-08-24 12:42:07 +0530

    Time: 8 ms (64.41%), Space: 10.3 MB (7.46%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int, int> mp;
+        int ans;
+        for(int i=0; i<nums.size(); i++)
+            mp[nums[i]]++;
+        
+        for(auto it: mp){
+            if(it.second==1){
+                ans=it.first;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
