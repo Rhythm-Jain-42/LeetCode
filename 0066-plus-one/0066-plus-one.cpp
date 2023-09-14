9c0e75438fe2a2ecaf50d141020b26e2ea05c5d1
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-09-14 17:31:35 +0530

    Time: 2 ms (44.49%), Space: 9.3 MB (8.60%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        for(int i=n-1; i>=0; i--) { 
+            if (digits[i] == 9){  
+              digits[i] = 0;
+            }
+            else{  
+                digits[i]+=1;
+                return digits;
+            }
+        }
+        
+        digits.push_back(0);
+        digits[0]=1;
+        return digits;
+    }
+};
