239a3c1aab3ad83e2f0998ca5d543858082f1ae1
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-09-16 12:36:25 +0530

    Time: 101 ms (89.56%), Space: 20.4 MB (36.25%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        priority_queue<pair<int, pair<int,int>>, 
+        vector<pair<int, pair<int,int>>>, greater<pair<int, pair<int,int>>>> pq;
+        
+        int n=heights.size();
+        int m=heights[0].size();
+        
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+        dist[0][0]=0;
+        pq.push({0, {0, 0}});
+        int dr[]={-1, 0, 1, 0};
+        int dc[]={0, 1, 0, -1};
+        while(!pq.empty())
+        {
+            auto it=pq.top();
+            pq.pop();
+            int diff=it.first;
+            int row=it.second.first;
+            int col=it.second.second;
+            
+            if(row==n-1 && col==m-1) return diff;
+            
+            for(int i=0; i<4; i++)
+            {
+                int newr=row+dr[i];
+                int newc=col+dc[i];
+                if(newr>=0 && newr<n && newc>=0 && newc<m)
+                {
+                    int neweff=max(abs(heights[row][col]-heights[newr][newc]), diff);
+                    if(neweff<dist[newr][newc])
+                    {
+                        dist[newr][newc]=neweff;
+                        pq.push({neweff, {newr, newc}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
