40802acc9a5ec20dc644f892e9a74eea24af9ab5
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-09-16 12:45:59 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (5.47%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div120-triangle/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div120-triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int f(int i, int j, vector<vector<int>>& triangle, int n, vector<vector<int>>& dp)
+    {
+        if(i==n-1) return triangle[n-1][j];
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int d=f(i+1, j, triangle, n, dp)+triangle[i][j]; //down
+        int dg=f(i+1, j+1, triangle, n, dp)+triangle[i][j]; //diagonal
+        
+        return dp[i][j]=min(d, dg);
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return f(0, 0, triangle, n, dp);
+    }
+};
