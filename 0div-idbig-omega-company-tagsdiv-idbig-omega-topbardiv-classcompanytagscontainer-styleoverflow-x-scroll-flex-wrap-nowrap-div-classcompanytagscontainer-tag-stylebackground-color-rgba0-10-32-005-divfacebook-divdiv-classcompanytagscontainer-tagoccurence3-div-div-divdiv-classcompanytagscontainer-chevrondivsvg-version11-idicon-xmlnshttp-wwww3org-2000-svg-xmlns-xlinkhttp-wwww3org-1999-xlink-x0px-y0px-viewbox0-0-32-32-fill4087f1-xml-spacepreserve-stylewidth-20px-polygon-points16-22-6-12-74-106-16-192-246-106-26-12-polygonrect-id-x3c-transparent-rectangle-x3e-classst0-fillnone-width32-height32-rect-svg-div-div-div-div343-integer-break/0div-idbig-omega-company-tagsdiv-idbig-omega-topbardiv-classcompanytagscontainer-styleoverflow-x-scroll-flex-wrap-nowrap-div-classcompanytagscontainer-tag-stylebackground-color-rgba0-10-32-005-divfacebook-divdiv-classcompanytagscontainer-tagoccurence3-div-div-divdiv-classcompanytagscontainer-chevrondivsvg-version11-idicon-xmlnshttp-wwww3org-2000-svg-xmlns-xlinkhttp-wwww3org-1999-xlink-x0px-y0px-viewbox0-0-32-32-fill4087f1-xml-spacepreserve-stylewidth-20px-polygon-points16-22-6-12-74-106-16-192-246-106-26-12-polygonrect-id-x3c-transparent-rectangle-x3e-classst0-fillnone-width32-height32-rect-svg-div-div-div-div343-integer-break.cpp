8e31d004537f8d884e35ba84b19e2beef149a542
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-10-06 12:30:25 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (22.36%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int f(int n, vector<int> &dp){
+        if(n==0) return 1;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        int maxi=INT_MIN;
+        for(int i=1; i<=n; i++)
+            maxi=max(maxi, i*f(n-i, dp));
+        
+        return dp[n]=maxi;
+    }
+    
+    int integerBreak(int n) {
+        if(n<=3) return n-1;
+        vector<int> dp(n+1, -1);
+        return f(n, dp);
+    }
+};
