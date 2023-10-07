efe64ae6a99c900d296c4594e1ba35c72b0eb661
Rhythm Jain <90083593+Rhythm-Jain-42@users.noreply.github.com>
2023-10-08 01:31:18 +0530

    Time: 232 ms (9.09%), Space: 10.6 MB (18.18%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    int solve(int idx,int prevCost,int prevMax,int n,int m,int k,vector<vector<vector<int>>>& mem){
+        if(prevCost > k){
+            return 0;
+        }
+        if(idx >= n){
+            //Travelled ALL indexes
+            if(prevCost == k){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+
+        if(mem[idx][prevMax][prevCost] != -1){
+            return mem[idx][prevMax][prevCost];
+        }
+
+        int ans = 0;
+
+        for(int i = 1;i <= m;i++){
+            if(i > prevMax){
+                ans = (ans + solve(idx+1,prevCost+1,i,n,m,k,mem)%mod)%mod;
+            }
+            else{
+                //Cost remains same as Max element is not alteted
+                //Note: i = prevMax, No more cost as already GOT Max previously
+                ans = (ans + solve(idx+1,prevCost,prevMax,n,m,k,mem)%mod)%mod;
+            }
+        }
+
+        return mem[idx][prevMax][prevCost] = ans;
+    }
+    int numOfArrays(int n, int m, int k) {
+        vector<vector<vector<int>>> mem(n+1,vector<vector<int>>(m+1,vector<int>(k+1,-1)));
+
+        return solve(0,0,0,n,m,k,mem);
+
+    }
+   
+};
